5c62845c8fd7ed7b6b474eb5d72ba6f2e0bf682a
kr.goodwilldd <Kr.GoodwillDD@gmail.com>
2013-03-07 12:11:20 +0000

    tiles3 추가 예정 thymlef 적용(멋짐!!) resource 에 관한 설정 ㅡ_ ㅡ 확인;; web.xml에서 mapping설정이 되어있어야함. json / xml을 명시적으로 받아들임(기본 xml)

---
+++ in-soft-prototype/src/main/java/kr/co/insoft/controller/main/model/User.java
@@ -0,0 +1,24 @@
+package kr.co.insoft.controller.main.model;
+
+
+public class User {
+	
+	private String firstName;
+	
+	private String lastName;
+	
+
+	public User(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+	
+}
